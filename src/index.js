5e3f65bb0152472a2927a527d40eb3b3ff641b13
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-03-01 11:34:23 +0200

    add-js-files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './scripts/header';
+import './scripts/category';
+import './scripts/cards-news';
+import './scripts/weather';
+import './scripts/pagination';
+import './scripts/favorite';
+import './scripts/read';
