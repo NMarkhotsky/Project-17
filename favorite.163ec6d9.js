91b4f70c5d20594f2bc1134faf04f0196e52c1c6
NMarkhotsky <111852661+NMarkhotsky@users.noreply.github.com>
2023-03-01 13:46:52 +0000

    Deploying to gh-pages from @ NMarkhotsky/Project-17@d68eebc6e3319a1386e3667d74e8f78ee88ab766 🚀

---
+++ favorite.163ec6d9.js
@@ -0,0 +1,2 @@
+[document.querySelector(".form"),document.querySelector(".form__input")][0].addEventListener("submit",(function(e){e.preventDefault()}));
+//# sourceMappingURL=favorite.163ec6d9.js.map
---
+++ favorite.927d7dc7.js
@@ -0,0 +1,2 @@
+({form:document.querySelector(".form"),input:document.querySelector(".form__input")}).form.addEventListener("submit",(function(e){e.preventDefault()}));
+//# sourceMappingURL=favorite.927d7dc7.js.map
