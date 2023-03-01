94211b1478b17ce9ecbd63346bcae4defa06555c
HaidaienkoK <gaidk1973@gmail.com>
2023-03-01 22:13:06 +0200

    card weather
    card weather, scss

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,54 +1,53 @@
-const { default: axios } = require("axios");
+const { default: axios } = require('axios');
 
 let weatherToday = new Date();
 let weatherDayOfWeek = weatherToday.toLocaleString('en-US', {
   weekday: 'long',
 });
-console.log(dayOfWeek);
+// console.log(weatherDayOfWeek);
 
 let weatherTodayOptions = { year: 'numeric', month: 'long', day: 'numeric' };
 let weatherDayNow = weatherToday.toLocaleString('en-GB', weatherTodayOptions);
-console.log(weatherDayNow);
+// console.log(weatherDayNow);
 
 const weatherContainer = document.querySelector('.weather');
+// console.log(weatherContainer);
+let latPosition = 0;
+let lonPosition = 0;
 
 function getCoordinat() {
   navigator.geolocation.getCurrentPosition(showCoordinat, showError);
 }
 
-function fetchElement() {
-    return axios.get('https://api.openweathermap.org/data/2.5/weather', {
-        params: {
-            lat: position.coords.latitude,
-            lon: position.coords.longitude,
-            appid: 'f2ba0fa18561e8523c95662543c65b15',
-            units: 'metric',
-        },
-    })
-        .then(response => response)
-        .then(data => {
-        console.log(data)
-        })
-        .catch(error => error);
-}
-
 function showCoordinat(position) {
-    
-    fetchElement();
-    cardsContainer.insertAdjacentHTML(
-      'beforeend',
-      `<div class="weather_UI">
+  latPosition = position.coords.latitude;
+  lonPosition = position.coords.longitude;
+  axios
+    .get('https://api.openweathermap.org/data/2.5/weather', {
+      params: {
+        lat: latPosition,
+        lon: lonPosition,
+        appid: 'f2ba0fa18561e8523c95662543c65b15',
+        units: 'metric',
+      },
+    })
+    .then(response => response)
+    .then(data => {
+      // console.log(data);
+      weatherContainer.insertAdjacentHTML(
+        'beforeend',
+        `<div class="weather_UI">
             <div class="weather_info">
                 <div class="weather_temperatura">
-                    <p class="weather_temp"> ${data.main.temp} </p>
+                    <p class="weather_temp"> ${Math.round(data.data.main.temp)} </p>
                     <p class="weather_badge">&#176</p>
                 </div>
                 <div class="weathr_position">
-                    <p class="weather_state">${data.weather.description}</p>
-                    <p class="weather_city">${data.name}</p>
+                    <p class="weather_state">${data.data.weather[0].description}</p>
+                    <p class="weather_city">${data.data.name}</p>
                 </div>
             </div>
-            <img class="weather_img" src="#" alt="weather img">
+            <img class="weather_img" src="https://openweathermap.org/img/wn/${data.data.weather[0].icon}@2x.png" alt="weather img">
             <div class="weather_day">
                 <p class="weather_dayOfWeek">${weatherDayOfWeek}</p>
                 <p class="weather_date">${weatherDayNow}</p>
@@ -56,11 +55,9 @@ function showCoordinat(position) {
         </div>
         <button class="weather_week">weather for week</button>
 `
-    );
-
-
-//   console.log("Широта:" + lat);
-//   console.log("Долгота:" + lon);
+      );
+    })
+    .catch(error => error);
 }
 
 function showError(error) {
@@ -81,4 +78,4 @@ function showError(error) {
 }
 
 // Запуск Геолокации
-// getCoordinat();
+getCoordinat();
