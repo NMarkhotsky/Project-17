d72b0f9eee839d85974a48e9f3d4cc324b16a426
HaidaienkoK <gaidk1973@gmail.com>
2023-03-02 17:52:25 +0200

    weather2 new cards

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -11,9 +11,14 @@ let weatherDayNow = weatherToday.toLocaleString('en-GB', weatherTodayOptions);
 // console.log(weatherDayNow);
 
 const weatherContainer = document.querySelector('.weather');
+let btnEl = '';
 // console.log(weatherContainer);
 let latPosition = 0;
 let lonPosition = 0;
+let tempsOnDay = [];
+
+  let arrDayObjects = [];
+
 
 function getCoordinat() {
   navigator.geolocation.getCurrentPosition(showCoordinat, showError);
@@ -62,6 +67,8 @@ function showCoordinat(position) {
         <button class="weather_week">weather for week</button>
 `
       );
+      btnEl = document.querySelector('.weather_week');
+      btnEl.addEventListener('click', onClickWeatherBtn);
     })
     .catch(error => error);
 }
@@ -81,6 +88,118 @@ function showError(error) {
       alert('An unspecified error has occurred');
       break;
   }
+}
+
+function oneStringToArr(str) {
+  return str.trim().split(' ');
+}
+
+function onClickWeatherBtn() {
+  //Создаем массив объекта з данными
+
+
+  weatherContainer.innerHTML = '';
+
+  console.log('Привет бтн');
+  // latPosition = position.coords.latitude;
+  // lonPosition = position.coords.longitude;
+  console.log(latPosition);
+  console.log(lonPosition);
+
+  axios
+    .get('https://api.openweathermap.org/data/2.5/forecast', {
+      params: {
+        lat: latPosition,
+        lon: lonPosition,
+        appid: 'f2ba0fa18561e8523c95662543c65b15',
+        units: 'metric',
+      },
+    })
+    .then(response => response)
+    .then(data => {
+      let arreyData = data.data.list;
+      let fullDays = [];
+      // console.log(arreyData);
+      // console.log(data.data.city);
+      weatherContainer.insertAdjacentHTML(
+        'beforeend',
+        `<div class="weather_UI_week">
+            <p class="weather_city_week">${data.data.city.name}</p>
+            <div class="weather_info_week">
+            </div>
+        </div>
+            `
+      );
+
+      const weatherConteinerOneDay = document.querySelector('.weather_info_week');
+      arreyData.forEach(element => {
+        dayAndTime = oneStringToArr(element.dt_txt);
+        fullDays.push(...oneStringToArr(dayAndTime[0]));
+      });
+      days = Array.from(new Set(fullDays));
+      for (i = 0; i < days.length; i += 1) {
+        tempsOnDay = [];
+        for (a = 0; a < arreyData.length; a += 1) {
+          if (oneStringToArr(arreyData[a].dt_txt)[0] === days[i]) {
+            tempsOnDay.push(arreyData[a].main.temp);
+          }
+        }
+        console.log(tempsOnDay);
+
+        // console.log(days[i]);
+        // let weatherTodayWeek = days[i];
+        // let weatherDayOfWeeks = weatherTodayWeek.toLocaleString('en-US', {
+        //   weekday: 'long',
+        // });
+        // console.log(weatherDayOfWeek);
+
+        // let weatherTodayOptionsWeek = {
+        //   year: 'numeric',
+        //   month: 'long',
+        //   day: 'numeric',
+        // };
+        // let weatherDayNowWeek = weatherToday.toLocaleString(
+        //   'en-GB',
+        //   weatherTodayOptionsWeek
+        // );
+        // console.log(weatherDayNow);
+  let myDayObject = {};
+        // arrDayObjects[i].dataDay = weatherDayNowWeek;
+        // arrDayObjects[i].weekDay = weatherDayOfWeeks;
+        mayDayObject[i].tempMin = Math.min(...tempsOnDay);
+        mayDayObject[i].tempMax = Math.max(...tempsOnDay);
+// arrDayObjects.push(mayDayObjects[i]);
+          // imgWeather: value5,
+    // };
+        console.log(arrDayObjects[i]);
+      }
+
+      
+      const markup = days
+        .map(
+          () =>
+            `<div class="weather_info_day">
+                    <p class="weather_dayOfWeek_week">Понедельник</p>
+                    <p class="weather_date_week">Дата</p>
+                    <img class="weather_img_week" src="#" alt="Погода картинка">
+                    <div class="weather_temp_fullday">
+                        <div class="weather_temperatura_min">
+                            <p class="weather_temp_week"> 0 </p>
+                            <p class="weather_badge_week">&#176</p>
+                        </div>
+                        <div class="weather_temperatura_max">
+                            <p class="weather_temp_week"> 0 </p>
+                            <p class="weather_badge_week">&#176</p>
+                        </div>
+                    </div>
+                </div>
+          `
+        )
+        .join('');
+
+      weatherConteinerOneDay.insertAdjacentHTML('beforeend', markup);
+    })
+    .catch(error => error);
 }
 
 // Запуск Геолокации
