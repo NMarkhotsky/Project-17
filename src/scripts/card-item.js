38919c8d729e0d6a941e9d6c6f9d2ff2f5414e58
OlhaSavva <olasavva@gmail.com>
2023-03-02 20:28:05 +0200

    finish card markup

---
+++ src/scripts/card-item.js
@@ -0,0 +1,79 @@
+import NewsApi from './API/newsAPI';
+
+const refs = {
+  cardList: document.querySelector('.cards__list'),
+};
+
+const newsApi = new NewsApi();
+
+function createMarkupForCard({
+  abstract,
+  published_date,
+  section,
+  title,
+  url,
+  imageUrl,
+  imageCaption,
+}) {
+  return `
+  <div class="card_item">
+    <div class="card_item-header">
+      <img class="card_item-image" src="${imageUrl}" alt="${imageCaption}" loading="lazy" />
+      <span class="card_item-section">${section}</span>
+      <button class="button__add-favorite" type="submit">
+        Add to favorite
+        <svg class="button__icon-svg" width="24" height="24">
+          <use href="src/img/symbol-defs.svg#icon-favorite"></use>
+        </svg>
+      </button>
+    </div>
+    <div class="cart_item-content">
+      <div class="card_item-text">
+        <h1 class="card_item-title">${title}</h1>
+        <p class="card_item-description">${abstract}</p>
+      </div>
+      <div class="card_item-info">
+        <span class="card_item-date">${published_date}</span>
+        <a class="card__link-btn" href="${url}" data-title="${title}">
+          <button class="button__read-more" type="submit">
+            Read more
+          </button>
+        </a>
+      </div>
+    </div>
+  </div>
+  `;
+}
+
+function newsAdapter(item) {
+  const { abstract, published_date, section, title, url, media } = item;
+
+  let imageUrl = 'https://via.placeholder.com/300x200';
+  let imageCaption = 'No image';
+
+  if (media?.length > 0) {
+    const mediaMetadata = media[0]?.['media-metadata'];
+    if (mediaMetadata?.length > 0) {
+      imageUrl = mediaMetadata[mediaMetadata.length - 1].url;
+    }
+  }
+
+  return {
+    abstract,
+    published_date,
+    section,
+    title,
+    url,
+    imageUrl,
+    imageCaption,
+  };
+}
+
+newsApi.fetchPopularNews().then(data => {
+  console.log(data);
+  const list = data.results
+    .map(item => createMarkupForCard(newsAdapter(item)))
+    .join('');
+
+  refs.cardList.innerHTML = list;
+});
--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,4 +1,4 @@
-const { default: axios } = require('axios');
+import axios from 'axios';
 
 let weatherToday = new Date();
 let weatherDayOfWeek = weatherToday.toLocaleString('en-US', {
@@ -39,15 +39,21 @@ function showCoordinat(position) {
         `<div class="weather_UI">
             <div class="weather_info">
                 <div class="weather_temperatura">
-                    <p class="weather_temp"> ${Math.round(data.data.main.temp)} </p>
+                    <p class="weather_temp"> ${Math.round(
+                      data.data.main.temp
+                    )} </p>
                     <p class="weather_badge">&#176</p>
                 </div>
                 <div class="weathr_position">
-                    <p class="weather_state">${data.data.weather[0].description}</p>
+                    <p class="weather_state">${
+                      data.data.weather[0].description
+                    }</p>
                     <p class="weather_city">${data.data.name}</p>
                 </div>
             </div>
-            <img class="weather_img" src="https://openweathermap.org/img/wn/${data.data.weather[0].icon}@2x.png" alt="weather img">
+            <img class="weather_img" src="https://openweathermap.org/img/wn/${
+              data.data.weather[0].icon
+            }@2x.png" alt="weather img">
             <div class="weather_day">
                 <p class="weather_dayOfWeek">${weatherDayOfWeek}</p>
                 <p class="weather_date">${weatherDayNow}</p>
@@ -63,16 +69,16 @@ function showCoordinat(position) {
 function showError(error) {
   switch (error.code) {
     case error.PERMISSION_DENIED:
-      alert('User prohibited from reading location information');
+      // alert('User prohibited from reading location information');
       break;
     case error.POSITION_UNAVAILABLE:
-      alert('The browser was unable to locate');
+      // alert('The browser was unable to locate');
       break;
     case error.TIMEOUT:
-      alert('Browser has not had time to locate');
+      // alert('Browser has not had time to locate');
       break;
     case error.UNKNOWN_ERROR:
-      alert('An unspecified error has occurred');
+      // alert('An unspecified error has occurred');
       break;
   }
 }
