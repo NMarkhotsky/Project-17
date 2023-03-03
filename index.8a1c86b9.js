e32f130b505d2ff9a6b03023d362eb0ab663709e
NMarkhotsky <111852661+NMarkhotsky@users.noreply.github.com>
2023-03-03 19:20:38 +0000

    Deploying to gh-pages from @ NMarkhotsky/Project-17@0e71c83429be394c4eef343bacc853371ccca97f 🚀

--- index.8a1c86b9.js
+++
@@ -1,2 +0,0 @@
-var refs={form:document.querySelector(".form"),input:document.querySelector(".form__input")};function onSubmit(e){e.preventDefault()}refs.form.addEventListener("submit",onSubmit);
-//# sourceMappingURL=index.8a1c86b9.js.map
