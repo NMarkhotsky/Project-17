e0d67fd82f26cfc5454f96509eb27f1da6cace9e
Marina Baronina <baronina.mi@gmail.com>
2023-03-04 15:19:39 +0200

    mobile

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -8,3 +8,20 @@ refs.form.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
 }
+
+(() => {
+  
+    const menuBtnRef = document.querySelector("[data-menu-button]");    
+    const mobileMenuRef = document.querySelector("[data-menu]");
+  
+
+  menuBtnRef.addEventListener("click",() => {
+    const expanded = 
+      menuBtnRef.getAttribute("aria-epanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-epanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-hidden");
+  });
+})();
