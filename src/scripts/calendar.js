7527bfb6a19421f882ae26ae862c18c8725e3a02
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-03-04 01:50:01 +0100

    updates calendar

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,21 +1,57 @@
 import flatpickr from 'flatpickr';
 
+const FLATPICKR_INPUT = document.querySelector('.flatpickr-input');
+const ARROW_BTN_DOWN = document.querySelector('.arrow-down');
+const ARROW_BTN_UP = document.querySelector('.arrow-up');
+const CALENDAR_ICON = document.querySelector('.calendar__button--left');
+const calendarIconSvg = new URL('../img/symbol-defs.svg', import.meta.url);
+
 const DATEPICKER_OPTIONS = {
-  // maxDate: 'today',
   wrap: true,
+  maxDate: 'today',
+  closeOnSelect: false,
+  nextArrow: `<svg class="flatpickr-icon flatpickr-icon--next">
+<use href="${calendarIconSvg}#icon-arrow-up"></use>
+<svg>`,
+  prevArrow: `<svg class="flatpickr-icon flatpickr-icon--prev">
+<use href="${calendarIconSvg}#icon-arrow-down"></use>
+<svg>`,
+  disable: [
+    function (date) {
+      return date > Date.now();
+    },
+  ],
   dateFormat: 'd/m/Y',
+  // positionElement: document.querySelector(".calendar-input"),
   position: 'below right',
   monthSelectorType: 'static',
-  // appendTo: document.querySelector(".flatpickr-container"),
-  // prevArrow: "<span title=\"Previous year\">&lt;&lt;</span>",
-  // nextArrow: "<span title=\"Next year\">&gt;&gt;</span>",
-  // onMonthChange(dateStr) {
-  // },
-  // onYearChange(dateStr) {
-  // },
+  onOpen() {
+    changeBtnStyles();
+  },
   onClose(dateStr) {
+    changeBtnStyles();
+    console.log(dateStr);
     return dateStr;
   },
 };
 
 const DATEPICKER = flatpickr('#flatpickr', DATEPICKER_OPTIONS);
+
+function changeBtnStyles() {
+  FLATPICKR_INPUT.classList.toggle('is-clicked');
+  ARROW_BTN_DOWN.classList.toggle('is-hidden');
+  ARROW_BTN_UP.classList.toggle('is-hidden');
+  CALENDAR_ICON.classList.toggle('is-clicked');
+}
+
+// function createYearInputStyles() {
+//   document.querySelector("arrow-up").innerHTML
+// }
+
+// let articlesArray = [{ name: 'article 1', published_date: "2023-03-04" }, { name: 'article 2', published_date: "2022-03-04" }, { name: 'article 3', published_date: "2020 -07-01" }];
+// let date = "2023-03-04";
+// function filterByDate(date, articlesArray) {
+//   let filtredArticles = articlesArray.filter(article => article.published_date === date);
+//   console.log (filtredArticles)
+// }
+// filterByDate(date, articlesArray)()
