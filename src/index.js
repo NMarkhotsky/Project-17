59864ec90768852ed18dcd0b2c173c9fb555e889
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-03-06 11:32:40 +0100

    updates index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import './scripts/API/fetchAPI';
 import './scripts/card-item';
 
 import './scripts/home'
+import './scripts/category'
+import './scripts/calendar';
 import './scripts/header';
 import './scripts/search-area';
 import './scripts/theme';
 
-import './scripts/category'
-import './scripts/calendar';
 import './scripts/pagination';
 import './scripts/weather';
 
