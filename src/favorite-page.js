72660032bbce6cb11c9984b0950eb5b5d236f3ca
Woodiik <vovikvova04@gmail.com>
2023-03-07 16:48:41 +0200

    test

--- src/favorite-page.js
+++ src/favorite-page.js
@@ -2,4 +2,5 @@ import './scripts/API/fetchAPI';
 import './scripts/header';
 import './scripts/card-item';
 import './scripts/favorite';
+import './scripts/favoriteBySearch';
 import './scripts/theme';
