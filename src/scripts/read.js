c82f0cd6d702660a7a08e7f13572284094e20bd9
OlhaSavva <olasavva@gmail.com>
2023-03-09 22:03:22 +0200

    add accordion to read page

--- src/scripts/read.js
+++ src/scripts/read.js
@@ -1,30 +1,44 @@
 import { createMarkupForCard, getRead } from './card-item';
 
 const refs = {
-  cardsRead: document.getElementById('ul-gallery'),
+  cardsRead: document.getElementById('read__page'),
 };
+
 const readPageCards = getRead();
 
 const listsByDate = Object.keys(readPageCards)
   .reverse()
-  .map(date => {
+  .map((date, i) => {
+    setTimeout(() => {
+      const acc = document.getElementsByClassName('accordion');
+      acc[i].addEventListener('click', function () {
+        /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+        this.classList.toggle('active');
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === 'flex') {
+          panel.style.display = 'none';
+        } else {
+          panel.style.display = 'flex';
+        }
+      });
+    }, 0);
+
     const list = Object.values(readPageCards[date])
       .map(item => {
         return createMarkupForCard(item);
       })
       .join('');
 
-    const accordion = `<div class="accordion">
-    <div class="accordion__header">
-      <div class="accordion__header-date">${date}</div>
-      <div class="accordion__header-icon"></div>
-    </div>
-    <div class="accordion__body">
-      <ul class="accordion__list">
-        ${list}
-      </ul>
-    </div>
-  </div>`;
+    const accordion = `
+    <div class="read-container">
+      <button class="accordion">${date}</button>
+      <div class="panel">
+          ${list}
+      </div>
+    </div>`;
 
     return accordion;
   })
