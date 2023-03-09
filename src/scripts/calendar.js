29bffc698ff22eef073e13bdec83545eeb0da363
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-03-09 18:32:37 +0100

    imorts-fix

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,6 +1,6 @@
 import flatpickr from 'flatpickr';
 import { popularNewsArray } from './home';
-import { categoriesNewsArray, clearActiveBtn, newsAdapter, createMarkupForCard } from './category';
+import { categoriesNewsArray, clearActiveBtn, newsAdapter, createMarkupForCards } from './category';
 
 const FLATPICKR_INPUT = document.querySelector('.flatpickr-input');
 const ARROW_BTN_DOWN = document.querySelector('.arrow-down');
@@ -102,7 +102,7 @@ function renderFiltredMarkupPopular(filtredArticles) {
     containerPagination.style = 'display: none';
   } else {
     const list = filtredArticles
-      .map(item => createMarkupForCard(newsAdapter(item)))
+      .map(item => createMarkupForCards(newsAdapter(item)))
       .join('');
     CARDS_LIST.innerHTML = list;
   }
@@ -116,7 +116,7 @@ function renderFiltredMarkupCategory(filtredArticles) {
     containerPagination.style = 'display: none';
   } else {
     const list = filtredArticles
-      .map(item => createMarkupForCard(newsAdapter(item)))
+      .map(item => createMarkupForCards(newsAdapter(item)))
       .join('');
     CARDS_LIST.innerHTML = list;
   }
